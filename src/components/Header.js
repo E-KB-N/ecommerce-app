ef037d0edd9587fd2580869798b55a6506ec1ee1
Edward Kojo Boateng Ntow <Kojoboateng514@gmail.com>
2023-04-14 19:36:34 +0000

    Header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,24 @@
-import React from "react"
-
-
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
 const Header = () => {
   return (
-    <div>Header</div>
-  )
+     <>
+  <header className="header-top-strip py-3">
+    <div className="container-xxl">
+      <div className="row">
+      <div className="col-6">
+        <p>Free Delivery for Purchases Over Ghc300</p>
+        </div>
+      <div className="col-6">
+        <p>
+          Hotline: <a href="tel:+233 2768 86105">+233 276886105 </a>
+        </p>
+      </div>
+    </div>
+    </div>
+    </header>
+    </>
+  );
 };
 
 export default Header;
