42bbdc9b839ef8a9121313061a0e68b7c35b4119
Edward Kojo Boateng Ntow <Kojoboateng514@gmail.com>
2023-04-15 04:01:58 +0000

    Did some more styling of the UI... will add some images to the homepage in the next commit.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
+import {BsSearch} from 'react-icons/bs';
 const Header = () => {
   return (
      <>
@@ -7,16 +8,71 @@ const Header = () => {
     <div className="container-xxl">
       <div className="row">
       <div className="col-6">
-        <p>Free Delivery for Purchases Over Ghc300</p>
+        <p className="text-white mb-0">
+          Free Delivery for Purchases Over Ghc300</p>
         </div>
       <div className="col-6">
-        <p>
-          Hotline: <a href="tel:+233 2768 86105">+233 276886105 </a>
+        <p className="text-end text-white mb-0">
+          Hotline: 
+          <a className="text-white" href="tel:+233 2768 86105">
+            +233 276886105 
+          </a>
         </p>
       </div>
     </div>
     </div>
     </header>
+    <header className="header-upper py-3">
+      <div className="container-xxl">
+        <div className="row align-items-center">
+          <div className="col-2">
+            <h2>
+              <Link className="text-white">House of EL.</Link>
+            </h2>
+          </div>
+          <div className="col-5">
+          <div className="input-group">
+            <input 
+              type="text"
+              class="form-control py-2" 
+              placeholder="Search for items..." 
+              aria-label="Search for items..." 
+              aria-describedby="basic-addon2"
+              />
+            <span className="input-group-text p-3" id="basic-addon2">
+              <BsSearch className="fs-6"/>
+            </span>
+          </div>
+          </div>
+          <div className="col-5"></div>
+            <div className="header-upper-links d-flex align-items-center justify-content-between"></div>
+              <div>
+                <Link>
+                  <img src="" alt=""/>
+                  <p></p>
+                </Link>
+              </div>
+              <div>
+                <Link>
+                 <img src="" alt=""/>
+                  <p></p>
+                </Link>
+              </div>
+              <div>
+                <Link>
+                  <img src="" alt=""/>
+                  <p></p>
+                </Link>
+              </div>
+              <div>
+                <Link>
+                  <img src="" alt=""/>
+                  <p></p>
+                </Link>
+              </div>
+        </div>
+      </div>
+    </header> 
     </>
   );
 };
