1772955a9285fb921c3162d988263a7a6b85bc81
Edward Kojo Boateng Ntow <Kojoboateng514@gmail.com>
2023-04-16 00:38:28 +0000

    Added icons to the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,78 +1,92 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
-import {BsSearch} from 'react-icons/bs';
+import { BsSearch } from "react-icons/bs";
 const Header = () => {
   return (
-     <>
-  <header className="header-top-strip py-3">
-    <div className="container-xxl">
-      <div className="row">
-      <div className="col-6">
-        <p className="text-white mb-0">
-          Free Delivery for Purchases Over Ghc300</p>
+    <>
+      <header className="header-top-strip py-3">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-6">
+              <p className="text-white mb-0">
+                Free Delivery for Purchases Over Ghc300
+              </p>
+            </div>
+            <div className="col-6">
+              <p className="text-end text-white mb-0">
+                Hotline:
+                <a className="text-white" href="tel:+233 2768 86105">
+                  +233 276886105
+                </a>
+              </p>
+            </div>
+          </div>
         </div>
-      <div className="col-6">
-        <p className="text-end text-white mb-0">
-          Hotline: 
-          <a className="text-white" href="tel:+233 2768 86105">
-            +233 276886105 
-          </a>
-        </p>
-      </div>
-    </div>
-    </div>
-    </header>
-    <header className="header-upper py-3">
-      <div className="container-xxl">
-        <div className="row align-items-center">
-          <div className="col-2">
-            <h2>
-              <Link className="text-white">House of EL.</Link>
-            </h2>
+      </header>
+      <header className="header-upper py-3">
+        <div className="container-xxl">
+          <div className="row align-items-center">
+            <div className="col-2">
+              <h2>
+                <Link className="text-white">House of EL.</Link>
+              </h2>
+            </div>
+            <div className="col-5">
+              <div className="input-group mb-3">
+                <input
+                  type="text"
+                  className="form-control py-2"
+                  placeholder="Search for items... "
+                  aria-label="Search for items... "
+                  aria-describedby="basic-addon2"
+                />
+                <span className="input-group-text p-3" id="basic-addon2">
+                  <BsSearch className="fs-6" />
+                </span>
+              </div>
+            </div>
+            <div className="col-5">
+              <div className="header-upper-links d-flex align-items-center justify-content-between">
+                <div>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                    <img src="images/compare.svg" alt="compare" />
+                    <p className="mb-0">
+                      Compare <br /> Products
+                    </p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                    <img src="images/wishlist.svg" alt="wishlist" />
+                    <p className="mb-0">
+                      Favourite <br /> wishlist
+                    </p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                    <img src="images/user.svg" alt="user" />
+                    <p className="mb-0">
+                      Log in <br /> My Account
+                    </p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                    <img src="images/cart.svg" alt="cart" />
+                    <div className="d-flex flex-column gap-10">
+                      <span className="badge bg-white text-dark">0</span>
+                      <p className="mb-0">
+                        <span>&#8373;</span> 300
+                      </p>
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="col-5">
-          <div className="input-group">
-            <input 
-              type="text"
-              class="form-control py-2" 
-              placeholder="Search for items..." 
-              aria-label="Search for items..." 
-              aria-describedby="basic-addon2"
-              />
-            <span className="input-group-text p-3" id="basic-addon2">
-              <BsSearch className="fs-6"/>
-            </span>
-          </div>
-          </div>
-          <div className="col-5"></div>
-            <div className="header-upper-links d-flex align-items-center justify-content-between"></div>
-              <div>
-                <Link>
-                  <img src="" alt=""/>
-                  <p></p>
-                </Link>
-              </div>
-              <div>
-                <Link>
-                 <img src="" alt=""/>
-                  <p></p>
-                </Link>
-              </div>
-              <div>
-                <Link>
-                  <img src="" alt=""/>
-                  <p></p>
-                </Link>
-              </div>
-              <div>
-                <Link>
-                  <img src="" alt=""/>
-                  <p></p>
-                </Link>
-              </div>
         </div>
-      </div>
-    </header> 
+      </header>
     </>
   );
 };
